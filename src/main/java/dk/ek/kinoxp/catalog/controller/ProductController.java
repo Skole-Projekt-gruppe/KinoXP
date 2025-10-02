41a33c146cea1b838b05e0a96d9a0f4e4639b066
Simon <simonsivebaek.hotmail@gmail.com>
2025-10-02 20:02:19 +0200

    shop virker dog mangler der en submit knap

--- src/main/java/dk/ek/kinoxp/catalog/controller/ProductController.java
+++ src/main/java/dk/ek/kinoxp/catalog/controller/ProductController.java
@@ -1,14 +1,25 @@
 package dk.ek.kinoxp.catalog.controller;
 
-
+import dk.ek.kinoxp.catalog.model.Product;
+import dk.ek.kinoxp.catalog.repository.ProductRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
-public class ProductController
-{
+@RequestMapping("/products")
+public class ProductController {
 
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
