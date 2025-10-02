76dad8f722f90d4d90c1176005302fd2c174d704
Simon <simonsivebaek.hotmail@gmail.com>
2025-10-02 19:08:03 +0200

    ProductService

--- src/main/java/dk/ek/kinoxp/catalog/service/ProductService.java
+++ src/main/java/dk/ek/kinoxp/catalog/service/ProductService.java
@@ -1,10 +1,10 @@
 package dk.ek.kinoxp.catalog.service;
 
-import dk.ek.kinoxp.catalog.dto.Mapper;
-import dk.ek.kinoxp.catalog.dto.MovieDto;
+import dk.ek.kinoxp.catalog.dto.ProductMapper;
+import dk.ek.kinoxp.catalog.dto.ProductDto;
 import dk.ek.kinoxp.catalog.exception.NotFoundException;
 import dk.ek.kinoxp.catalog.model.Product;
-import dk.ek.kinoxp.catalog.repository.MovieRepository;
+import dk.ek.kinoxp.catalog.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.sql.Array;
@@ -15,4 +15,27 @@ import java.util.List;
 public class ProductService
 {
 
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository)
+    {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductDto> getAllProducts()
+    {
+        List<Product> products = productRepository.findAll();
+
+        if (products.isEmpty())
+        {
+            throw new NotFoundException("No products found");
+        }
+
+        List<ProductDto> productDtos = new ArrayList<>();
+        for(var product : products)
+        {
+            productDtos.add(ProductMapper.toDto(product));
+        }
+        return productDtos;
+    }
 }
