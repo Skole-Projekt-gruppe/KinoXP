67edf556ce3e3800007a24b2c29377fd4bbea703
Simon <simonsivebaek.hotmail@gmail.com>
2025-10-06 16:14:11 +0200

    Staff på vej

---
+++ src/main/java/dk/ek/kinoxp/catalog/controller/StaffController.java
@@ -0,0 +1,26 @@
+package dk.ek.kinoxp.catalog.controller;
+
+import dk.ek.kinoxp.catalog.model.Staff;
+import dk.ek.kinoxp.catalog.repository.StaffRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/staff")
+@CrossOrigin
+public class StaffController
+{
+
+    private final StaffRepository repo;
+
+    public StaffController(StaffRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public List<Staff> getAllStaff() {
+        return repo.findAll();
+    }
+
+}
