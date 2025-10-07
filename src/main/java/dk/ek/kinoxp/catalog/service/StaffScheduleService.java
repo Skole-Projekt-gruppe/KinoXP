57df004ed0a92085ee4101742d1a23db75fe98e0
Simon <simonsivebaek.hotmail@gmail.com>
2025-10-07 15:10:35 +0200

    kan nu se medarbjedere

--- src/main/java/dk/ek/kinoxp/catalog/service/StaffScheduleService.java
+++ src/main/java/dk/ek/kinoxp/catalog/service/StaffScheduleService.java
@@ -1,4 +1,35 @@
 package dk.ek.kinoxp.catalog.service;
 
-public class StaffScheduleService {
+import dk.ek.kinoxp.catalog.dto.StaffScheduleMapper;
+import dk.ek.kinoxp.catalog.dto.StaffScheduleDto;
+import dk.ek.kinoxp.catalog.exception.NotFoundException;
+import dk.ek.kinoxp.catalog.model.StaffSchedule;
+import dk.ek.kinoxp.catalog.repository.StaffScheduleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaffScheduleService
+{
+    private final StaffScheduleRepository staffScheduleRepository;
+
+    public StaffScheduleService(StaffScheduleRepository staffScheduleRepository) {
+        this.staffScheduleRepository = staffScheduleRepository;
+    }
+
+    public List<StaffScheduleDto> getAllStaffSchedules() {
+        List<StaffSchedule> staffSchedules = staffScheduleRepository.findAll();
+
+        if (staffSchedules.isEmpty()) {
+            throw new NotFoundException("No staff schedules found");
+        }
+
+        List<StaffScheduleDto> staffScheduleDtos = new ArrayList<>();
+        for(var schedule : staffSchedules) {
+            staffScheduleDtos.add(StaffScheduleMapper.toDto(schedule));
+        }
+        return staffScheduleDtos;
+    }
+
 }
--- src/main/java/dk/ek/kinoxp/catalog/service/StaffService.java
+++ src/main/java/dk/ek/kinoxp/catalog/service/StaffService.java
@@ -1,6 +1,36 @@
 package dk.ek.kinoxp.catalog.service;
 
+import dk.ek.kinoxp.catalog.dto.StaffMapper;
+import dk.ek.kinoxp.catalog.dto.StaffDto;
+import dk.ek.kinoxp.catalog.exception.NotFoundException;
+import dk.ek.kinoxp.catalog.model.Staff;
+import dk.ek.kinoxp.catalog.repository.StaffRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StaffService
 {
-    
+    private final StaffRepository staffRepository;
+
+    public StaffService(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
+    }
+
+    public List<StaffDto> getAllStaff() {
+        List<Staff> staff = staffRepository.findAll();
+
+        if (staff.isEmpty()) {
+            throw new NotFoundException("No staff found");
+        }
+
+        List<StaffDto> staffDtos = new ArrayList<>();
+        for(var member : staff) {
+            staffDtos.add(StaffMapper.toDto(member));
+        }
+        return staffDtos;
+    }
+
+
 }
